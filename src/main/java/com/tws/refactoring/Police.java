c00d8f5d2b2c2390097a6d906b78490eef6600a0
Locki Lo <locki.lo@cargosmart.com>
2018-11-26 15:20:39 +0800

    Sucessfully refactored unclear parameter in Police

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -2,6 +2,7 @@ package com.tws.refactoring;
 
 public class Police {
     final static int validAgeForDriving = 18;
+
     public boolean checkDriver(Driver driver) {
         return driver.getAge() >= validAgeForDriving;
     }
